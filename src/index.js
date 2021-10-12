49edc7adc7fa5300fa47eae24943c3cf487deb20
Pawel Milczak <pawel.milczak@monogo.pl>
2021-10-12 14:47:20 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
+import { BuilderComponent } from "@builder.io/react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <div className="logo">MY SITE</div>
+        <div className="links">
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/page-1">
+            Page 1
+          </Link>
+          <Link className="link" to="/page-2">
+            Page 2
+          </Link>
+        </div>
+      </header>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route
+            render={({ location }) => <CatchallPage key={location.key} />}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+class CatchallPage extends React.Component {
+  state = { notFound: false };
+
+  render() {
+    return !this.state.notFound ? (
+      <BuilderComponent
+        apiKey="99875dd864da4bb4a7828dc46015430f"
+        model="page"
+        contentLoaded={(content) => {
+          if (!content) {
+            this.setState({ notFound: true });
+          }
+        }}
+      >
+        <div className="loading">Loading...</div>
+      </BuilderComponent>
+    ) : (
+      <NotFound /> // Your 404 content
+    );
+  }
+}
+
+const Home = () => <h1>I am the homepage!</h1>;
+const About = () => <h1>I am the about page!</h1>;
+const NotFound = () => <h1>No page found for this URL, did you publish it?</h1>;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
